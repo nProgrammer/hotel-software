08a99d0dcdd085fda092a882912e386ca0b3ba32
nProgrammer <wagner.norbert277@gmail.com>
2020-01-20 21:32:53 +0100

    UI updated

---
+++ client-platform/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div id="main">
+            <input type="text" placeholder="USERNAME LOGIN" id="login"/>
+            <br/>
+            <br/>
+            <input type="password" placeholder="PASSWORD" id="pass"/>
+            <br/>
+            <br/>
+        </div>
+    </div>
+  );
+}
+export default App;
